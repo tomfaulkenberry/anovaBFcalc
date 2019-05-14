d15ce12bfd9796db20395a08be09201cb28659ee
tomfaulkenberry <tomfaulkenberry@gmail.com>
2019-05-14 05:53:34 -0500

    initial commit

---
+++ server.R
@@ -0,0 +1,74 @@
+
+shinyServer(function(input, output) { 
+  
+  # get input and calculate Bayes factor
+  
+  values = reactiveValues()
+  
+  observe({
+    if (input$repeated == "bet"){
+      values$N <- input$df1 + input$df2 + 1
+      values$BF01 <- sqrt(values$N^(input$df1)*(1+input$F*input$df1/input$df2)^(-values$N))
+    }
+    else {
+      values$k <- input$df1 + 1
+      values$n <- (input$df2 + input$df1)/input$df1 
+      values$BF01 <- sqrt((values$n*values$k-values$n)^(values$k-1)*(1+input$F/(values$n-1))^(values$n-values$n*values$k))
+    }
+    
+    values$pOdds = input$pH0/(1-input$pH0)
+  })
+  
+ 
+  # render outputs (plot and text)
+  
+  # Output 1 - bayes factor "pizza" plot
+  output$pizza <- renderPlot({
+ 
+    library(plotrix)
+    xPos = 0.25
+    yPos = 0.75
+    radius = 0.12
+    A = pi*radius^2
+    alpha = 2/(1/values$BF01+1)*A/radius^2
+    startpos = pi/2 - alpha/2
+    plotrix::floating.pie(xPos,yPos,c(values$BF01,1),radius=radius,col=c("#4F2D7F","white"),lwd=2,startpos=startpos)
+    text(xPos, yPos+1.2*radius, "data|H0", cex=1.5, font=2)
+    text(xPos, yPos-1.22*radius, "data|H1", cex=1.5, font=2)
+    },
+    
+    width=800,
+    height=800
+    )
+  
+  # Output 2 - text describing Bayes factor
+  output$bfNull <- renderText({
+    sprintf("The Bayes factor for the null is %.2f", values$BF01)
+  })
+  
+  output$bfAlt <- renderText({
+    sprintf("The Bayes factor for the alternative is %.2f", 1/values$BF01)
+  })
+  
+  output$description <- renderText({
+    if(values$BF01 > 1){
+      sprintf("The observed data is approximately %.2f times more likely under the null than the alternative", values$BF01)
+      }
+    else{
+      sprintf("The observed data is approximately %.2f times more likely under the alternative than the null", 1/values$BF01)
+      
+      }
+    })
+    
+  # Output 3 - text describing posterior probabilities
+  output$postNull <- renderText({
+    sprintf("The posterior probability for the null is %.4f", (values$BF01*values$pOdds)/(1+(values$BF01*values$pOdds)))
+  })
+  
+  output$postAlt <- renderText({
+    sprintf("The posterior probability for the alternative is %.4f", 1-(values$BF01*values$pOdds)/(1+(values$BF01*values$pOdds)))
+  })
+  
+    
+  }
+)
